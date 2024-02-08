a743d7d6a3d8a04d4d703aa1db34216d9a20dac2
Christian Bechstedt <chrisnebech@hotmail.com>
2024-02-08 15:42:48 -0300

    dev-start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react'
+import Logo from './components/Logo';
+import Arrow from './components/Arrow';
+import { Button } from 'react-bootstrap';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className='header-container'>
+        <Logo />
+        <nav className='menu-container'>
+          <a>What we offer</a>
+          <a>How it works</a>
+          <a>Portfolio</a>
+        </nav>
+        <Button className='btn-purple'>
+          Contact us
+        </Button>
       </header>
-    </div>
+    </main>
   );
-}
+};
+
+
 
 export default App;
